50ff8e209a1baccaf05da62da7aff7e43becc02f
Artem Kushner <fokusnik38@gmail.com>
2024-12-13 18:40:23 +0300

    "все по заданию 20 модуля"

--- app/src/main/java/com/kushner/myapplication/MainActivity.kt
+++ app/src/main/java/com/kushner/myapplication/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.kushner.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kushner.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.buttons1.setOnClickListener {
+            Toast.makeText(this, "МЕНЮ", Toast.LENGTH_SHORT).show()
         }
+        binding.buttons2.setOnClickListener {
+            Toast.makeText(this, "ИЗБРАННОЕ", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttons3.setOnClickListener {
+            Toast.makeText(this, "ПОСМОТРЕТЬ ПОЗЖЕ", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttons4.setOnClickListener {
+            Toast.makeText(this, "ПОДБОРКИ", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttons5.setOnClickListener {
+            Toast.makeText(this, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
+        }
+        }
+
     }
-}
